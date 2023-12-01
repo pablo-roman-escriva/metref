45fc5a4befc339446afe5b193a67cdc26520cf4a
Pablo Román-Escrivá <pabloromanescriva@gmail.com>
2023-12-01 13:09:46 +0100

    feat - add taxid

--- taxonomy/models.py
+++ taxonomy/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Taxonomy(models.Model):
-    tax_id = models.PositiveIntegerField(default=0)
+    tax_id = models.PositiveIntegerField(default=0, primary_key=True)
     parent_tax_id = models.PositiveIntegerField(default=0)
     name = models.CharField(max_length=200, default='')
     unique_name = models.CharField(max_length=200, default='')
