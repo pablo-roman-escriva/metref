838a24335e611e1ef845dfbcc3b83540d4ca809c
Pablo Román-Escrivá <pabloromanescriva@gmail.com>
2023-12-01 13:50:54 +0100

    feat - add versioning db

--- taxonomy/models.py
+++ taxonomy/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
-class Taxonomy(models.Model):
+class Version(models.Model):
+    name = models.CharField(max_length=200,default='')
+    date = models.DateField()
+    status = models.PositiveSmallIntegerField(default=1)
+
+class Taxon(models.Model):
     tax_id = models.PositiveIntegerField(default=0, primary_key=True)
     parent_tax_id = models.PositiveIntegerField(default=0)
     name = models.CharField(max_length=200, default='')
     unique_name = models.CharField(max_length=200, default='')
     rank = models.CharField(max_length=200, default='')
-    version = models.PositiveSmallIntegerField(default=0)
+    version = models.ManyToManyField(Version)
 
-    class Meta:
-        db_table = "taxonomy"
